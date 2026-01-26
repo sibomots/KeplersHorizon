6ac78cd2bf777dc95391a031a6e1eca581d0ee71
spinoff <spinoff@protonmail.com>
2026-01-26 15:55:21 +0000

    restyled CRT

--- site/server/misc/src/crt_command.cpp
+++ site/server/misc/src/crt_command.cpp
@@ -13,31 +13,31 @@
 
 bool CrtCommand::invoke(void)
 {
-    std::ostringstream out;
-    out << "         COMBAT RESULTS TABLE\n";
-    out << "===========================================\n";
-    out << " Firing  | Drive  | vs ATK | vs DOD | vs RET\n";
-    out << "---------+--------+--------+--------+--------\n";
-    out << " ATTACK  | <=-3   | Miss   | Miss   | Escapes\n";
-    out << " ATTACK  | -2,-1  | Hit    | Miss   | Escapes\n";
-    out << " ATTACK  |  0,+1  | Hit+2  | Miss   | Miss   \n";
-    out << " ATTACK  | +2     | Hit+1  | Hit+1  | Miss   \n";
-    out << " ATTACK  | +3,+4  | Miss   | Hit    | Hit    \n";
-    out << " ATTACK  | >=+5   | Miss   | Miss   | Miss   \n";
-    out << "---------+--------+--------+--------+--------\n";
-    out << " DODGE   | <=-4   | Miss   | Miss   | Escapes\n";
-    out << " DODGE   | -3,-2  | Miss   | Hit    | Escapes\n";
-    out << " DODGE   | -1,0   | Hit    | Hit    | Escapes\n";
-    out << " DODGE   | +1,+2  | Hit    | Miss   | Escapes\n";
-    out << " DODGE   | >=+3   | Miss   | Miss   | Escapes\n";
-    out << "---------+--------+--------+--------+--------\n";
-    out << " RETREAT | <=-2   | Miss   | Miss   | Escapes\n";
-    out << " RETREAT | -1,0   | Hit    | Miss   | Escapes\n";
-    out << " RETREAT | >=+1   | Miss   | Miss   | Escapes\n";
-    out << "===========================================\n";
-    out << "Damage = Beam Power + Tech Level\n";
-    out << "         (may be modified by hex events)\n";
+    static constexpr const char crt_table[] =
+           "              COMBAT RESULTS TABLE\n"
+           "┌────────┬─────────┬────────┬──────────┬────────┐\n"
+           "│Tactic  │ Drive Δ │ vs ATT │ vs DODGE │ vs RET │\n"
+           "├────────┼─────────┼────────┼──────────┼────────┤\n"
+           "│ATTACK  │ <=-3    │ Miss   │ Miss     │ Escapes│\n"
+           "│ATTACK  │ -2,-1   │ Hit    │ Miss     │ Escapes│\n"
+           "│ATTACK  │  0,+1   │ Hit+2  │ Miss     │ Miss   │\n"
+           "│ATTACK  │ +2      │ Hit+1  │ Hit+1    │ Miss   │\n"
+           "│ATTACK  │ +3,+4   │ Miss   │ Hit      │ Hit    │\n"
+           "│ATTACK  │ >=+5    │ Miss   │ Miss     │ Miss   │\n"
+           "├────────┼─────────┼────────┼──────────┼────────┤\n"
+           "│DODGE   │ <=-4    │ Miss   │ Miss     │ Escapes│\n"
+           "│DODGE   │ -3,-2   │ Miss   │ Hit      │ Escapes│\n"
+           "│DODGE   │ -1,0    │ Hit    │ Hit      │ Escapes│\n"
+           "│DODGE   │ +1,+2   │ Hit    │ Miss     │ Escapes│\n"
+           "│DODGE   │ >=+3    │ Miss   │ Miss     │ Escapes│\n"
+           "├────────┼─────────┼────────┼──────────┼────────┤\n"
+           "│RETREAT │ <=-2    │ Miss   │ Miss     │ Escapes│\n"
+           "│RETREAT │ -1,0    │ Hit    │ Miss     │ Escapes│\n"
+           "│RETREAT │ >=+1    │ Miss   │ Miss     │ Escapes│\n"
+           "└────────┴─────────┴────────┴──────────┴────────┘\n"
+           "      Damage = Beam Power + Tech Level\n"
+           "     >> may be modified by hex events << \n";
 
-    Telemetry::instance().write(out.str());
+    Telemetry::instance().write(crt_table);
     return true;
 }
